11379e65b1179eab4f04aa287c02acc74d8850ee
Dong Tran <thiendong.iuh@gmail.com>
2024-04-28 20:52:08 +0700

    fix ui blink

--- frontend/app/components/recent-posts-card/recent-posts-card.tsx
+++ frontend/app/components/recent-posts-card/recent-posts-card.tsx
@@ -5,26 +5,12 @@ import { Tag } from "../tag/tag";
 import classes from "./recent-posts-card.module.css";
 import { useNavigate } from "@remix-run/react";
 
-interface Post {
-  id: string;
-  title: string;
-  banner_image: string;
-  params: {
-    tags: string[];
-  };
-}
-
-interface ApiResponse {
-  content: Post[];
-}
-
 export function RecentPostsCard() {
-  const [data, setData] = useState<ApiResponse | null>(null);
+  const [data, setData] = useState(null);
   const [selectedTag, setSelectedTag] = useState<string>("All");
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log("import.meta.env.VITE_API_URL", import.meta.env.VITE_API_URL);
     fetch(
       `${
         import.meta.env.VITE_API_URL
@@ -32,11 +18,40 @@ export function RecentPostsCard() {
     )
       .then((response) => response.json())
       .then((data) => {
-        setData(data);
+        const recentPosts = data?.content
+          ?.filter(
+            (item: {
+              id: string;
+              title: string;
+              banner_image: string;
+              params: { tags: string[] };
+            }) => {
+              const tags = item?.params?.tags;
+
+              if (
+                selectedTag?.toLowerCase() === "all" &&
+                tags?.includes("trending")
+              ) {
+                return false;
+              }
+              return true;
+            }
+          )
+          ?.map((post: { id: string; title: string; banner_image: string }) => (
+            <RecentPost
+              key={post.id}
+              title={post.title}
+              bannerImage={post.banner_image}
+              onClick={() => handlePostClick(post.id)}
+            />
+          ));
+
+        setData(recentPosts);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedTag]);
 
   const handlePostClick = (id: string) => {
@@ -72,26 +87,7 @@ export function RecentPostsCard() {
         />
       </div>
 
-      {data &&
-        data?.content
-          ?.filter((item) => {
-            const tags = item?.params?.tags;
-            if (
-              selectedTag?.toLowerCase() === "all" &&
-              tags?.includes("trending")
-            ) {
-              return false;
-            }
-            return true;
-          })
-          ?.map((post: { id: string; title: string; banner_image: string }) => (
-            <RecentPost
-              key={post.id}
-              title={post.title}
-              bannerImage={post.banner_image}
-              onClick={() => handlePostClick(post.id)}
-            />
-          ))}
+      {data}
     </Paper>
   );
 }
