be18cfe8f220e9983dc6457120fcc6bd9c29d4bb
Dong Tran <thiendong.iuh@gmail.com>
2024-04-29 00:00:20 +0700

    add react-ga

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,20 +4,12 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dotenv from "dotenv";
 import { VitePluginRadar } from "vite-plugin-radar";
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-GPVG26MR6B');
+import ReactGA from "react-ga";
+ReactGA.initialize("G-GPVG26MR6B");
 
 dotenv.config();
 installGlobals();
 
 export default defineConfig({
-  plugins: [
-    remix(),
-    tsconfigPaths(),
-    VitePluginRadar({
-      analytics: {
-        id: 'G-GPVG26MR6B',
-      },
-    })
-  ],
+  plugins: [remix(), tsconfigPaths()],
 });
