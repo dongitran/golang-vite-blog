214162b44f941fe2021bd618f7a201b0d3591600
Dong Tran <thiendong.iuh@gmail.com>
2024-04-28 23:36:34 +0700

    update analytics

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -22,53 +22,7 @@ export default defineConfig({
       // Google Analytics (multiple tag can be set with an array)
       analytics: [
         {
-          /**
-           * Measurement id
-           */
           id: "G-GPVG26MR6B",
-
-          /**
-           * disable tracking for this measurement
-           *   window['ga-disable-MEASUREMENT_ID'] = true
-           * @see https://developers.google.com/analytics/devguides/collection/ga4/disable-analytics
-           */
-          disable: true,
-
-          /**
-           * You can configure all settings provided by analytics here
-           * @see https://developers.google.com/analytics/devguides/collection/ga4/cookies-user-id
-           * @see https://developers.google.com/analytics/devguides/collection/ga4/disable-page-view
-           * @see https://developers.google.com/analytics/devguides/collection/ga4/display-features
-           */
-          config: {
-            cookie_domain: "auto",
-            cookie_expires: 63072000,
-            cookie_prefix: "none",
-            cookie_update: true,
-            cookie_flags: "",
-            send_page_view: true,
-            allow_google_signals: true,
-            allow_ad_personalization_signals: true,
-          },
-
-          /**
-           * Set default values for "consent mode"
-           * @see https://developers.google.com/tag-platform/devguides/consent
-           * @see https://support.google.com/analytics/answer/9976101
-           */
-          consentDefaults: {
-            analytics_storage: "granted",
-            ad_storage: "denied",
-            wait_for_update: 500,
-          },
-
-          /**
-           * You set persitent values
-           * @see https://developers.google.com/analytics/devguides/collection/ga4/persistent-values
-           */
-          persistentValues: {
-            currency: "USD",
-          },
         },
         // You can add as many measurement id as you need
         {
