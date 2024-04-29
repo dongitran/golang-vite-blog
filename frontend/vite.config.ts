09ba885053cc9600017398f9bcad3195a4f67bdd
Dong Tran <thiendong.iuh@gmail.com>
2024-04-29 17:06:07 +0700

    create ingress for backend service and update env frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dotenv from "dotenv";
-import { VitePluginRadar } from "vite-plugin-radar";
 import ReactGA from "react-ga";
 ReactGA.initialize("G-GPVG26MR6B");
 
